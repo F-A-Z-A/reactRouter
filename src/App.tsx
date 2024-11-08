dc0de96e4d2fa9eacdac078491acbd63d8ae86be
AndreyUrazaev <f.a.z.a@bk.ru>
2024-11-08 15:55:49 +0500

    Lesson-01-05

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PageOne } from "./components/pages/PageOne";
+import { PageTwo } from "./components/pages/PageTwo";
+import { PageThree } from "./components/pages/PageThree";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
+import { Error404 } from "./components/pages/Error404";
+import { S } from "./components/pages/_styles";
 
-function App() {
+const PATH = {
+  ADIDAS: "/page1",
+  PUMA: "/page2",
+  ABIBAS: "/page3",
+  ERROR: "/error",
+} as const;
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <S.Header>
+        <h1>HEADER</h1>
+      </S.Header>
+      <S.Body>
+        <S.Nav>
+          <S.NavWrapper>
+            <NavLink to={PATH.ADIDAS}> Adidas </NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.PUMA}> Puma </NavLink>
+          </S.NavWrapper>
+          <S.NavWrapper>
+            <NavLink to={PATH.ABIBAS}> Abibas </NavLink>
+          </S.NavWrapper>
+        </S.Nav>
+        <S.Content>
+          <Routes>
+            <Route path={"/"} element={<Navigate to={PATH.ADIDAS} />} />
+
+            <Route path={PATH.ADIDAS} element={<PageOne />} />
+            <Route path={PATH.PUMA} element={<PageTwo />} />
+            <Route path={PATH.ABIBAS} element={<PageThree />} />
+
+            <Route path={"/*"} element={<Error404 />} />
+            {/*<Route path={PATH.ERROR} element={<Error404 />} />*/}
+            {/*<Route path={"/*"} element={<Navigate to={PATH.ERROR} />} />*/}
+          </Routes>
+        </S.Content>
+      </S.Body>
+      <S.Footer> abibas 2024 </S.Footer>
     </div>
   );
 }
-
-export default App;
